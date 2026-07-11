LineageOS/android_device_oneplus_sm8350-common

sm8350-common: Use legacy libion implementation
* At least one blob directly depends on this, with
  many other bits and pieces likely.

Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

BoardConfigCommon.mk
@@ -144,6 +144,7 @@ BOOT_SECURITY_PATCH := 2025-04-01
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
+include device/lineage/sepolicy/libion/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 

MistOS-Devices/device_xiaomi_sm8350-common

BoardConfigCommon.mk
@@ -161,6 +161,7 @@ VENDOR_SECURITY_PATCH := 2025-11-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/statix/sepolicy/libion/sepolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public